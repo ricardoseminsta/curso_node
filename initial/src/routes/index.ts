0ffee5692818eb9175f1e65b60cb1b0b94c30b70
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-06-24 10:06:28 -0300

    commit mustache set and configurations

--- initial/src/routes/index.ts
+++ initial/src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-    res.send("Ola! Mundo");
+    res.render('home');
 });
 
 router.get('/contato', (req: Request, res: Response) => {
--- initial/src/server.ts
+++ initial/src/server.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from "express";
 import path from "path";
-
+import mustache from "mustache-express";
 import mainRoutes from './routes/index';
 import panelRoutes from './routes/panel';
 
 
 const server = express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache()); 
 
 server.use('/static', express.static(path.join(__dirname, '../public')));
 
