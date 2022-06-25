7d493b76be9d41f0a24a0d76b2a2d788c2c3b327
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-06-25 11:09:18 -0300

    commit loob list exibition

--- initial/src/routes/index.ts
+++ initial/src/routes/index.ts
@@ -7,7 +7,13 @@ router.get('/', (req: Request, res: Response) => {
         name: 'Ricardo',
         age: 25,
         showWelcome: true,
-        showOld: true
+        showOld: true,
+        products: [
+            {title:'Produto X', price:10},
+            {title:'Produto Y', price:20},
+            {title:'Produto Z', price:22}
+        ],
+        skills: ['brave', 'mustache', 'faster', 'express']
     }
 
     user.showOld = user.age > 30 ? true : false;
