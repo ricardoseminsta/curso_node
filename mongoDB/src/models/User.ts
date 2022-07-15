a0cd352877ee79925a60ac3adc3da48e1ffc624f
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-07-15 11:12:05 -0300

    commit adjustments type user.ts and querys mongo

---
+++ mongoDB/src/models/User.ts
@@ -0,0 +1,27 @@
+import { Schema, model, Model, connection } from "mongoose";
+
+type UserType = {
+    email: string;
+    age: number;
+    interests: [string];
+    name: {
+        firstName: string;
+        lastName: string;   
+    }
+}
+
+const schema = new Schema<UserType>({
+    email: { type: String, required: true }, 
+    age: { type: Number, required: true},
+    interests: [String],
+    name: {
+        firstName: { type: String, required: true},
+        lastName: String
+    }
+});
+
+const modelName: string = 'User';
+
+const userModel = (connection && connection.models[modelName]) ? connection.models[modelName] as Model<UserType> : model<UserType>(modelName, schema);
+
+export default userModel;
