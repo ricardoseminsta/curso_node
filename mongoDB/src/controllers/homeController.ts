635ceb617eda967091543e1674469a60601e26e8
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-07-16 11:21:40 -0300

    commit Create new user

--- mongoDB/src/controllers/homeController.ts
+++ mongoDB/src/controllers/homeController.ts
@@ -4,11 +4,14 @@ import User from '../models/User';
 
 export const home = async (req: Request, res: Response)=>{
 
-    let usuarios = await User.find({
-            age: { $gte: 2 }
-    }).sort({ "name.firstName": 1 }).skip(1).limit(1);
+    let newUser = await User.create({
+        name: { firstName: "Vick", lastName: "Fernandes"},
+        email: "vick@gmail.com",
+        age: 3,
+        interests: ["eat", "care", "sleep"]
+    });
 
-    console.log('USUARIOS', usuarios);
+    console.log('NOVO USUER', newUser);
 
     let age: number = 90;
     let showOld: boolean = false;
