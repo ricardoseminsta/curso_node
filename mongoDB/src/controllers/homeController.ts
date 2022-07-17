d744d8f3c547ee15a23e458a4a5d6a767eac7bbc
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-07-16 22:54:15 -0300

    commit remove user

--- mongoDB/src/controllers/homeController.ts
+++ mongoDB/src/controllers/homeController.ts
@@ -3,8 +3,10 @@ import { Product } from '../models/Product';
 import User from '../models/User';
 
 export const home = async (req: Request, res: Response)=>{
-    await User.findOneAndDelete({email: 'mingau@gmail.com'});
-
+    let user = await User.findOne({email: 'preta@gmail.com'});
+    if(user){
+        await user.remove();
+    }   
     let users = await User.find({}).sort({"name.firstName": 1});
 
     res.render('pages/home', {
