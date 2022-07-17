0a07f64c98e76600ced0c0e075952411a4a09fe4
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-07-16 22:48:27 -0300

    commit find one and save

--- mongoDB/src/controllers/homeController.ts
+++ mongoDB/src/controllers/homeController.ts
@@ -3,10 +3,12 @@ import { Product } from '../models/Product';
 import User from '../models/User';
 
 export const home = async (req: Request, res: Response)=>{
-    await User.updateOne(
-        { email: "ricardo@gmail.com" },
-        { age: 45 }
-    );
+    let ricardo = await User.findOne({ email: 'ricardo@gmail.com'});
+    if(ricardo) {
+    ricardo.name.lastName = 'Ferreira';
+    ricardo.age = 35;
+    await ricardo.save();
+    }
 
     let users = await User.find({}).sort({"name.firstName": 1});
 
