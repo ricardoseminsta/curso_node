713330375227d7c322dc932d711bf66aa81ced47
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-07-16 21:41:07 -0300

    commit exercise insert new user

--- mongoDB/src/controllers/userController.ts
+++ mongoDB/src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import User from '../models/User';
 
 export const nome = (req: Request, res: Response) => {
     let nome: string = req.query.nome as string;
@@ -29,4 +30,26 @@ export const idadeAction = (req: Request, res: Response) => {
         idade,
         mostrarIdade
     });
+};
+
+export const novoUsuario = async (req: Request, res: Response) => {
+    let newUser = new User();
+    newUser.name = {firstName: req.body.firstName, lastName: req.body.lastName}
+    newUser.email = req.body.email;
+    newUser.age = parseInt(req.body.age);
+    newUser.interests = req.body.interests.split(',');
+    if(newUser.name.firstName && newUser.email && newUser.name.lastName && newUser.age && newUser.interests) {
+        try {
+            let result = await newUser.save();
+            console.log("Usuario inserdo com sucesso!");
+            console.log("Usuario ", result);
+            
+        } catch (error) {
+            console.log("erro ao insrerir Usuario ", error);      
+        }
+} else {
+    console.log("Preencha todos os campos");
+    
+}
+    res.redirect('/')
 };
--- mongoDB/src/routes/index.ts
+++ mongoDB/src/routes/index.ts
@@ -14,5 +14,6 @@ router.get('/sobre', InfoController.sobre);
 router.get('/nome', UserController.nome);
 router.get('/idade', UserController.idadeForm);
 router.post('/idade-resultado', UserController.idadeAction);
+router.post('/novousuario', UserController.novoUsuario)
 
 export default router;
