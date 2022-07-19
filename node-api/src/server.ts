bf35cfccddf3547affd380518bd730f15030e749
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-07-18 23:03:59 -0300

    commit simple endpoints

--- node-api/src/server.ts
+++ node-api/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request , Response } from "express";
 import path from "path";
 import dotenv from "dotenv";
+import apiRoutes from "./routes/api"; 
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const server = express();
 
 server.use(express.static(path.join(__dirname, "../public")));
 server.use(express.urlencoded({extended: true}));
+
+server.use(apiRoutes)
 
 server.use((req: Request, res: Response) => {
     res.status(404);
