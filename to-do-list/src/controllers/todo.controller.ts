2446a18d04ac60fad626f9a246decbadae2e91aa
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-07-22 23:04:32 -0300

    create new item new task

--- to-do-list/src/controllers/todo.controller.ts
+++ to-do-list/src/controllers/todo.controller.ts
@@ -11,8 +11,17 @@ export const all = async (req: Request, res: Response) => {
 }
 
 export const add = async (req: Request, res: Response) => {
+    if(req.body.title) {
+        let newTask = await Todo.create({ 
+            title: req.body.title,
+            done: req.body.done ? true : false
+        });
+        res.status(201).json({ item: newTask });
 
-}
+    } else {
+        res.json({ error: "Data not send" })
+    }
+} 
 
 export const update = async (req: Request, res: Response) => {
 
