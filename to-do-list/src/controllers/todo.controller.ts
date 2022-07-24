6ad68aa89bd477950d698c29008c2143800fd6ab
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-07-23 23:13:57 -0300

    Todo Update and remove

--- to-do-list/src/controllers/todo.controller.ts
+++ to-do-list/src/controllers/todo.controller.ts
@@ -24,9 +24,37 @@ export const add = async (req: Request, res: Response) => {
 } 
 
 export const update = async (req: Request, res: Response) => {
+    let id: string = req.params.id;
 
+    let todo = await Todo.findByPk(id);
+    if(todo) {
+        if(req.body.title) {
+            todo.title = req.body.title;
+        }
+        if(req.body.done) {
+            switch(req.body.done.toLowerCase()) {
+                case 'true':
+                case '1':
+                    todo.done = true;
+                    break
+                case 'false':
+                case '0':
+                    todo.done = false;
+                    break
+            }
+        }
+        await todo.save();
+        res.json({ item: todo })
+    } else {
+        res.json({error: "Item Not Found"});
+    }
 }
 
 export const remove = async (req: Request, res: Response) => {
-
+    let id = req.params.id;
+    let todo  = await Todo.findByPk(id);
+    if(todo) {
+        await todo.destroy();
+    }
+    res.json({});
 }
