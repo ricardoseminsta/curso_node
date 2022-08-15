be859879911e035ca49e0e119d8976f1b316a0a8
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-08-14 21:59:42 -0300

    basic auth ok

--- node-api-base-auth/src/middlewares/auth.ts
+++ node-api-base-auth/src/middlewares/auth.ts
@@ -1,10 +1,29 @@
 import { Request, Response, NextFunction } from "express";
+import { User } from "../models/User";
 
 export const Auth  = {
-    private: (req: Request, res: Response, next: NextFunction) => {
+    private: async (req: Request, res: Response, next: NextFunction) => {
         //fazer verificação de auth
         let success = false;
-
+        if(req.headers.authorization) {
+            let hash:string = req.headers.authorization.substring(6);
+            let decoded: string = Buffer.from(hash, 'base64').toString();
+            let data: string[] = decoded.split(':');
+            
+            if(data.length === 2){
+                let hasUser = await User.findOne({
+                    where: {
+                        email: data[0],
+                        password: data[1]
+                    }
+                });
+                if(hasUser) {
+                    success = true;
+                }
+            }
+            
+        }
+        
         if(success) {
             next();
         } else {
