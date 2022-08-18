ca33642e668d1c0ba7b96555363221f3fc88c08a
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-08-18 16:40:23 -0300

    token validation

--- node-api-base-auth/src/middlewares/auth.ts
+++ node-api-base-auth/src/middlewares/auth.ts
@@ -1,27 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import { User } from "../models/User";
+import JWT from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const Auth  = {
     private: async (req: Request, res: Response, next: NextFunction) => {
         //fazer verificação de auth
         let success = false;
+        
         if(req.headers.authorization) {
-            let hash:string = req.headers.authorization.substring(6);
-            let decoded: string = Buffer.from(hash, 'base64').toString();
-            let data: string[] = decoded.split(':');
-            
-            if(data.length === 2){
-                let hasUser = await User.findOne({
-                    where: {
-                        email: data[0],
-                        password: data[1]
-                    }
-                });
-                if(hasUser) {
+            const [authType, token] = req.headers.authorization.split(' ');
+            if(authType === 'Bearer') {
+                try {
+                    const decoded = JWT.verify(
+                        token,
+                        process.env.JWT_SECRET_KEY as string
+                    );
+                    console.log("DECODED", decoded);
                     success = true;
+                } catch (error) {
                 }
             }
-            
         }
         
         if(success) {
