749efb30713595ab381938414e4eca88184c5a77
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-09-19 21:16:35 -0300

    endpoint tests
    endpoint tests and boy parser error and controller return error

---
+++ node-base-api-test/src/routes/api.test.ts
@@ -0,0 +1,35 @@
+import { response } from 'express';
+import request from 'supertest';
+import app from '../app';
+import { User } from '../models/User';
+
+describe('testing api routes', () => {
+
+    let email: string = 'email@example.com';
+    let password: string = 'password';
+
+    beforeAll(async () => {
+        await User.sync({ force: true });
+    });
+
+    it('should ping pong', (done) => {
+        request(app)
+            .get('/ping')
+            .then(response => {
+                expect(response.body.pong).toBeTruthy();
+                return done();
+            });
+    });
+
+    it('should register a new user', (done) => {
+        request(app)
+            .post('/register')
+            .send(`email=${email}&password=${password}`)
+            .then(response => {
+                expect(response.body.error).toBeUndefined();
+                expect(response.body).toHaveProperty('id');
+                return done();
+            });
+    });
+
+});
