3af7208ee6a2e1225502c64f7e547f073b2ed060
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2023-01-06 22:25:57 -0300

    chat node message change ok

--- chatnode/public/main.js
+++ chatnode/public/main.js
@@ -1,13 +1,13 @@
 const socket = io();
 
 let username = '';
-let userlist = [];
+let userList = [];
 
 let loginPage = document.querySelector('#loginPage');
 let chatPage = document.querySelector('#chatPage');
 
 let loginInput = document.querySelector('#loginNameInput');
-let texInput = document.querySelector('#chatTextInput');
+let textInput = document.querySelector('#chatTextInput');
 
 loginPage.style.display = 'flex';
 chatPage.style.display = 'none';
@@ -15,9 +15,24 @@ chatPage.style.display = 'none';
 function renderUserList() {
     let ul = document.querySelector('.userList');
     ul.innerHTML = '';
-    userlist.forEach(i => {
+    
+    userList.forEach(i => {
         ul.innerHTML += `<li>${i}</li>`;
     });
+}
+
+function addMessage(type, user, msg) {
+    let ul = document.querySelector('.chatList');
+
+    switch (type) {
+        case 'status':
+            ul.innerHTML += '<li class="m-status">' + msg + '</li>'; 
+        break;
+        case 'msg':
+            ul.innerHTML += '<li class="m-txt"><span>' + user + ' </span>' + msg + '</li>';
+        break;
+    
+    }    
 }
 
 loginInput.addEventListener('keyup', (e)=>{
@@ -33,12 +48,41 @@ loginInput.addEventListener('keyup', (e)=>{
     }
 });
 
+textInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
+        let txt = textInput.value.trim();
+        textInput.value = '';
+        
+        if(txt != '') {
+            addMessage('msg', username, txt);
+            socket.emit('send-msg', txt);
+        }
+    }
+});
+
 socket.on('user-ok', (list) => {
     loginPage.style.display = 'none';
     chatPage.style.display = 'flex';
+    textInput.focus();
 
-    texInput.focus();
+    addMessage('status', null, 'Connected!')
 
-    userlist = list;
+    userList = list;
     renderUserList();
+});
+
+
+socket.on('list-update', (data) => {
+    if(data.joined) {
+        addMessage('status', null, data.joined + ' Entrou no Chat.');
+    }
+    if(data.left) {
+        addMessage('status', null, data.left + ' Saiu no Chat.');
+    }
+    userList = data.list;
+    renderUserList();
+});
+
+socket.on('show-msg', (data) => {
+    addMessage('msg', data.username, data.message);
 });
